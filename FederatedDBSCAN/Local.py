f6496536e263402aac388e2bd29dfcccfbb1bb83
Alessio <alessio.ser@hotmail.it>
2021-02-03 14:55:44 +0100

    Local.py approssimativamente fatto

---
+++ FederatedDBSCAN/Local.py
@@ -0,0 +1,39 @@
+from scipy.io import arff
+import numpy as np
+import math
+
+L = 0.01
+MIN_PTS = 4
+
+if __name__ == "__main__":
+    data, meta = arff.loadarff('banana.arff')
+
+    points = []
+
+    for row in data:
+        points.append([math.floor(row[0] / L), math.floor(row[1] / L)])
+
+    arrPoints = np.array(points)
+
+    print(arrPoints)
+
+    maxX = np.amax(arrPoints[:, 0])
+    maxY = np.amax(arrPoints[:, 1])
+
+    count_matrix = np.zeros((maxX + 1, maxY + 1))
+    print(count_matrix.shape)
+
+    for x, y in arrPoints:
+        count_matrix[x][y] += 1
+        print(f'elemento [{x}][{y}] : {count_matrix[x][y]}')
+
+    list_to_return = []
+    for x in range(maxX):
+        for y in range(maxY):
+            if count_matrix[x][y] >= MIN_PTS:
+                list_to_return.append((x, y))
+
+    for i in list_to_return:
+        print(f'elemento {i} contiene: {count_matrix[i[0]][i[1]]} elementi')
+
+    print(f'celle dense totali: {len(list_to_return)}')
