8e8e1b7813fa21dfaeb9d9c383cf4464d067a4b9
ValeGian <46gianninivalerio@gmail.com>
2021-02-04 14:55:34 +0100

    Main update

--- FederatedDBSCAN/main.py
+++ FederatedDBSCAN/main.py
@@ -1,5 +1,18 @@
 from utils import *
+import Local as node
 
 if __name__ == '__main__':
-    partitionDataset()
+    M = int(input("Insert the number of nodes: "))
+    #partitionDataset(M)
 
+    contribuitionMap = {}
+    for i in range(M):
+        #localUpdate = node.computeLocalUpdate(i)
+        localUpdate = {(20, 55): 5, (20, 56): 2, (20, 57):1, (20, 62):3, (20, 66):1, (21, 52):9}
+        for key, value in localUpdate.items():
+            if key in contribuitionMap:
+                contribuitionMap[key] += value
+            else:
+                contribuitionMap[key] = value
+    print(contribuitionMap)
+    #clusters = computeClusters(localUpdates)
