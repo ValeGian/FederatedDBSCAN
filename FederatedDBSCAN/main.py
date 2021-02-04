1f13d4b6cb8ca6bfaa47bfe4924b4b396461e8db
ValeGian <46gianninivalerio@gmail.com>
2021-02-04 11:31:20 +0100

    Separated Method v3

--- FederatedDBSCAN/main.py
+++ FederatedDBSCAN/main.py
@@ -1,8 +1,5 @@
 from utils import *
 
 if __name__ == '__main__':
-    arr = obtainSuddivision(3,8)
-    print(arr)
-    #partitionDataset()
-    #removePartitions()
+    partitionDataset()
 
--- FederatedDBSCAN/utils.py
+++ FederatedDBSCAN/utils.py
@@ -12,11 +12,11 @@ PARTITIONS_PATH = "./partitions/"
 PARTITIONING_METHODS = ["stratified", "separated", "partially_separated"]
 
 def partitionDataset(M = 2):
+    removePartitions()
     #print(f'Choose the dataset to be partitioned:\n{os.listdir(DATASETS_PATH)}')
     #file = DATASETS_PATH + input()
     #print()
     file = DATASETS_PATH + "banana.arff"
-    #file = PARTITIONS_PATH + "partition0.arff"
     data, meta = arffsc.loadarff(file)
 
     #print('Choose the partitioning method:')
@@ -46,13 +46,24 @@ def partitionDataset(M = 2):
             separatedL[classes.index(elem[-1])].append(elem)
 
         if M >= N:
+            sub = obtainSubdivision(N, M)
+            count = 0
             for i in range(N):
-                for j in range(M//N):
-                    lowerB = j * len(separatedL[i]) // (M//N)
-                    upperB = (j+1) * len(separatedL[i]) // (M//N)
+                for j in range(sub[i]):
+                    lowerB = j * len(separatedL[i]) // sub[i]
+                    upperB = (j+1) * len(separatedL[i]) // sub[i]
                     arr = np.array(separatedL[i][lowerB:upperB])
                     df = pd.DataFrame(arr)
-                    dumpArff(df, i * M // N + j)
+                    dumpArff(df, count)
+                    count += 1
+        else:
+            rng = np.random.default_rng()
+            rng.shuffle(data)
+            for i in range(M):
+                lowerB = i * data.size // M
+                upperB = (i + 1) * data.size // M
+                df = pd.DataFrame(data[lowerB:upperB])
+                dumpArff(df, i)
         print(datetime.now() - start)
     #elif partitioningIndex == 2:
     #    i = 2
@@ -79,9 +90,8 @@ def removePartitions():
         os.remove(f'{PARTITIONS_PATH}{file}')
 
 
-def obtainSuddivision(num_class , num_node ):
+def obtainSubdivision(num_class, num_node):
     arr_to_return = np.full(num_class, math.floor(num_node/num_class))
     for i in range(num_node % num_class):
         arr_to_return[i] += 1
-
     return arr_to_return
